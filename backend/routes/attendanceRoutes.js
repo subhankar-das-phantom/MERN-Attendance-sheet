bd82561f847f84185b50baa9f9e183deaaf587ce
subhankar-das-phantom <subhankarnew1@gmail.com>
2026-03-21 20:26:29 +0530

    Update attendanceRoutes.js

--- backend/routes/attendanceRoutes.js
+++ backend/routes/attendanceRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { markAttendance, updateAttendance, getAllAttendance, getSummary } = require('../controllers/attendanceController');
+const { markAttendance, updateAttendance, getAllAttendance, getSummary, getStudentAttendance } = require('../controllers/attendanceController');
 
 router.post('/attendance', markAttendance);
 router.put('/attendance/:dateStr', updateAttendance);
 router.get('/attendance', getAllAttendance);
 router.get('/summary', getSummary);
+router.get('/students/:id/attendance', getStudentAttendance);
 
 module.exports = router;
